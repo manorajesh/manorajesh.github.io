18e0107c7cf68c1a3559aa2ee6d6bed98de962ea
manorajesh <mano@raje.sh>
2023-02-26 10:33:46 -0800

    Iterating and importing some models

--- main.js
+++ main.js
@@ -1,67 +1,47 @@
 import * as THREE from 'three';
-import './style.css'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-// Scene
-const scene = new THREE.Scene();
+// Loop through each model canvas and render the 3D models
+const modelCanvases = document.querySelectorAll('canvas');
+modelCanvases.forEach(function(canvas, index) {
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(canvas.parentElement.clientWidth, canvas.parentElement.clientHeight);
+  const scene = new THREE.Scene();
 
-// Create Sphere
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-  color: 0x00ff83,
-  roughness: 0.7,
-  metalness: 0.5,
+  // Set up the camera
+  const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
+  camera.position.set(3, 3, 3);
+  scene.add(camera);
+
+  // Set up the lighting
+  const light = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(light);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  directionalLight.position.set(0, 1, 0);
+  scene.add(directionalLight);
+
+  // Load the 3D model
+  const loader = new GLTFLoader();
+  loader.load('./models/model' + (index + 1) + '.glb', function(gltf) {
+    gltf.scene.position.set(0, 0, 0);
+    scene.add(gltf.scene);
+
+    // Set up the isometric view
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 0, 0);
+    controls.enablePan = false;
+    controls.minDistance = 2;
+    controls.maxDistance = 10;
+    controls.update();
+    controls.addEventListener('change', function() {
+      renderer.render(scene, camera);
+    });
+
+    // Render the scene
+    renderer.render(scene, camera);
+  });
+
+  // Add the canvas to the DOM
+  canvas.appendChild(renderer.domElement);
 });
-const mesh = new THREE.Mesh(geometry, material);
-
-// Add Sphere to Scene
-scene.add(mesh);
-
-// Sizes
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-// Camera
-const camera = new THREE.PerspectiveCamera(50, sizes.width / sizes.height, 0.1, 1000);
-camera.position.z = 20;
-scene.add(camera);
-
-// Lights
-const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-pointLight.position.set(10, 10, 10);
-scene.add(pointLight);
-
-// Renderer
-const canvas = document.getElementById("webgl");
-const renderer = new THREE.WebGLRenderer({antialias: true, canvas });
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-controls.enablePan = false;
-controls.enableZoom = false;
-controls.autoRotate = true;
-controls.autoRotateSpeed = 5;
-
-// Resize
-window.addEventListener('resize', () => {
-  // Update Sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update Camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
-});
-
-const loop = () => {
-  controls.update();
-  window.requestAnimationFrame(loop);
-  renderer.render(scene, camera);
-}
-loop();
