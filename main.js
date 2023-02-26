a71efaf35dfaf60d186d6b14aa1f274be5c3804e
manorajesh <mano@raje.sh>
2023-02-26 10:53:33 -0800

    Scrollable list now works

--- main.js
+++ main.js
@@ -1,47 +1,43 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import "./style.css"
 
-// Loop through each model canvas and render the 3D models
-const modelCanvases = document.querySelectorAll('canvas');
-modelCanvases.forEach(function(canvas, index) {
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(canvas.parentElement.clientWidth, canvas.parentElement.clientHeight);
-  const scene = new THREE.Scene();
+// Create a scene
+const scene = new THREE.Scene();
 
-  // Set up the camera
-  const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
-  camera.position.set(3, 3, 3);
-  scene.add(camera);
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-  // Set up the lighting
-  const light = new THREE.AmbientLight(0xffffff, 0.5);
-  scene.add(light);
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-  directionalLight.position.set(0, 1, 0);
-  scene.add(directionalLight);
+// get array of model divs
+const modelCanvas = document.querySelectorAll('.model');
+modelCanvas.forEach(canvas => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-  // Load the 3D model
+  // import model from models/models.glb
   const loader = new GLTFLoader();
-  loader.load('./models/model' + (index + 1) + '.glb', function(gltf) {
-    gltf.scene.position.set(0, 0, 0);
+  loader.load('models/model1.glb', function (gltf) {
+    // resize gltf to fit in div 
+    gltf.scene.scale.set(0.5, 0.5, 0.5);
     scene.add(gltf.scene);
-
-    // Set up the isometric view
-    const controls = new OrbitControls(camera, canvas);
-    controls.target.set(0, 0, 0);
-    controls.enablePan = false;
-    controls.minDistance = 2;
-    controls.maxDistance = 10;
-    controls.update();
-    controls.addEventListener('change', function() {
-      renderer.render(scene, camera);
-    });
-
-    // Render the scene
-    renderer.render(scene, camera);
+  }, undefined, function (error) {
+    console.error(error);
   });
 
-  // Add the canvas to the DOM
-  canvas.appendChild(renderer.domElement);
+
+  // Add a light
+  const light = new THREE.PointLight(0xffffff, 1, 100);
+  light.position.set(0, 0, 10);
+  scene.add(light);
+
+  // Render the scene
+  function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  animate();
 });
